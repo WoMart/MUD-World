c3423dec88cc2ea0eeb8533752d2e4f084ea4a7e
WoMart <m.woznicki.16@aberdeen.ac.uk>
2019-03-20 01:52:45 +0000

    Launching server is now absolutely beautiful

--- src/mud/MUDWorldServer.java
+++ src/mud/MUDWorldServer.java
@@ -1,21 +1,32 @@
 package mud;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import java.rmi.RemoteException;
 
 
 public class MUDWorldServer {
 
+    private static int getPort(String s) {
+        System.out.print(s + " port: ");
+        try { return new Scanner( System.in ).nextInt(); }
+        catch (InputMismatchException ignored)
+        { return getPort(s); }
+    }
+
     public static void main(String[] args) throws RemoteException {
 
-        int registry = 0;
-        int server = 0;
+        int registry;
+        int server;
         try {
             registry = Integer.parseInt(args[0]);
             server = Integer.parseInt(args[1]);
         }
         catch (IndexOutOfBoundsException | NumberFormatException ignored) {
-            System.out.println("Failed to start the game.\nRequired arguments: <hostname> <server port>\n");
-            System.exit(0);
+            System.out.println();
+            registry = getPort("Registry");
+            server = getPort("Server");
         }
 
         new Server(registry, server);
